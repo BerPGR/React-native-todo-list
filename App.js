82acfb230fb3de3390ee6ec48d6d2bcd506fee8f
Bernardo Matuchewski <bernardoPGR01@gmail.com>
2022-09-06 16:04:36 -0300

    Write a task section

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 import Task from './components/Task';
 
@@ -8,18 +8,54 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#E8EAED',
   },
+
   tasksWrapper: {
-    paddingTop: 60,
+    paddingTop: 80,
     paddingHorizontal: 20,
   },
+  
   sectionTitle: {
     fontSize: 24,
     fontWeight: 'bold',
     color: '#000'
   },
+  
   items: {
     marginTop: 30,
-    
+  },
+  
+  writeTaskWrapper: {
+    justifyContent:'space-around',
+    flexDirection: 'row',
+    alignItems: 'center',
+    position: 'absolute',
+    width: '100%',
+    bottom: 30,
+  },
+  
+  input: {
+    backgroundColor: '#fff',
+    borderColor: '#C6C6C6',
+    paddingHorizontal: 15,
+    paddingVertical: 15,
+    borderRadius: 60,
+    borderWidth: 1,
+    width: 250,
+  },
+  
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: '#fff',
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#C6C6C6',
+    borderWidth: 1,
+  },
+  
+  addText: {
+
   },
 });
 
@@ -36,6 +72,18 @@ export default function App() {
           <Task text={'Task 2'}/>
         </View>
       </View>
+
+      {/* Write a task */}
+      <KeyboardAvoidingView 
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      style={styles.writeTaskWrapper}>
+        <TextInput style={styles.input} placeholder={'Write a task'}/>
+        <TouchableOpacity >
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
 
     </View>
   );
